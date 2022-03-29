6268b81e706052d537e04dfcc257ef8085c3a06a
Mrinal Karmokar <mrinalkarmokar007@gmail.com>
2022-03-29 21:21:08 +0530

    first commit

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -80,11 +80,11 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-    'default': dj_database_url.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+    # 'default': dj_database_url.config()
 }
 
 
